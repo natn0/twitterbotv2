742083353820a58669f29c55b62e884b72487b5b
Anton <kemenist@hotmail.com>
2018-06-24 23:43:52 +0200

    Add project files.

---
+++ twitterbotv2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace twitterbotv2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
